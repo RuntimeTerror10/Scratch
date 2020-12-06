0288a2718eb36eb88744df00803f81cfb3dba2fd
RuntimeTerror10 <53986600+RuntimeTerror10@users.noreply.github.com>
2020-12-06 12:57:10 +0530

    stop preloader when site is loaded.

---
+++ preloader.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('.preloader');
+    preloader.classList.add('preloader-finish');
+})
+
+window.addEventListener('load', () => {
+    const an = document.querySelector('.no_ani');
+    an.classList.add('circles');
+})
