323a27cbf0297159370548b394b04a1a6cf3a8ef
RuntimeTerror10 <53986600+RuntimeTerror10@users.noreply.github.com>
2020-12-08 15:04:22 +0530

    flex page code toggle

---
+++ flex.js
@@ -0,0 +1,143 @@
+var show;
+function show_hide() {
+    if (show == 1) {
+        document.getElementById("row-rev").style.display = "none";
+        return show = 0;
+    }
+    else
+        document.getElementById("row-rev").style.display = "block";
+        return show = 1;
+
+}
+var show1;
+function show_hide1() {
+    if (show1 == 1) {
+        document.getElementById("row").style.display = "none";
+        return show1 = 0;
+    }
+    else
+        document.getElementById("row").style.display = "block";
+        return show1 = 1;
+
+}
+var show2;
+function show_hide3() {
+    if (show2 == 1) {
+        document.getElementById("col").style.display = "none";
+        return show2 = 0;
+    }
+    else
+        document.getElementById("col").style.display = "block";
+        return show2 = 1;
+
+}
+var show3;
+function show_hide4() {
+    if (show3 == 1) {
+        document.getElementById("col-rev").style.display = "none";
+        return show3 = 0;
+    }
+    else{
+        document.getElementById("col-rev").style.display = "block";
+        return show3 = 1;
+    }   
+}
+var show4;
+function show_hide5() {
+    if (show4 == 1) {
+        document.getElementById("start").style.display = "none";
+        return show4 = 0;
+    }
+    else{
+        document.getElementById("start").style.display = "block";
+        return show4 = 1;
+    }   
+}
+var show5;
+function show_hide6() {
+    if (show5 == 1) {
+        document.getElementById("end").style.display = "none";
+        return show5 = 0;
+    }
+    else{
+        document.getElementById("end").style.display = "block";
+        return show5 = 1;
+    }   
+}
+var show6;
+function show_hide7() {
+    if (show6 == 1) {
+        document.getElementById("center").style.display = "none";
+        return show6 = 0;
+    }
+    else{
+        document.getElementById("center").style.display = "block";
+        return show6 = 1;
+    }   
+}
+var show7;
+function show_hide8() {
+    if (show7 == 1) {
+        document.getElementById("btw").style.display = "none";
+        return show7 = 0;
+    }
+    else{
+        document.getElementById("btw").style.display = "block";
+        return show7 = 1;
+    }   
+}
+var show8;
+function show_hide9() {
+    if (show8== 1) {
+        document.getElementById("aro").style.display = "none";
+        return show8 = 0;
+    }
+    else{
+        document.getElementById("aro").style.display = "block";
+        return show8 = 1;
+    }   
+}
+var show9;
+function show_hide10() {
+    if (show9== 1) {
+        document.getElementById("even").style.display = "none";
+        return show9 = 0;
+    }
+    else{
+        document.getElementById("even").style.display = "block";
+        return show9 = 1;
+    }   
+}
+var show10;
+function show_hide11() {
+    if (show10== 1) {
+        document.getElementById("ali-start").style.display = "none";
+        return show10 = 0;
+    }
+    else{
+        document.getElementById("ali-start").style.display = "block";
+        return show10 = 1;
+    }   
+}
+var show11;
+function show_hide12() {
+    if (show11== 1) {
+        document.getElementById("ali-center").style.display = "none";
+        return show11 = 0;
+    }
+    else{
+        document.getElementById("ali-center").style.display = "block";
+        return show11 = 1;
+    }   
+}
+var show12;
+function show_hide13() {
+    if (show12== 1) {
+        document.getElementById("ali-end").style.display = "none";
+        return show12 = 0;
+    }
+    else{
+        document.getElementById("ali-end").style.display = "block";
+        return show12 = 1;
+    }   
+}
