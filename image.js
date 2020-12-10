bbe95aa8a324dffcf8362796b7a60117ead07356
RuntimeTerror10 <53986600+RuntimeTerror10@users.noreply.github.com>
2020-12-10 09:51:58 +0530

    image  code toggle

---
+++ image.js
@@ -0,0 +1,99 @@
+var show;
+function show_hide1() {
+    if (show == 1) {
+        document.getElementById("grey").style.display = "none";
+        return show = 0;
+    }
+    else
+        document.getElementById("grey").style.display = "block";
+        return show = 1;
+
+}
+var show1;
+function show_hide2() {
+    if (show1 == 1) {
+        document.getElementById("blur").style.display = "none";
+        return show1 = 0;
+    }
+    else
+        document.getElementById("blur").style.display = "block";
+        return show1 = 1;
+
+}
+var show2;
+function show_hide3() {
+    if (show2 == 1) {
+        document.getElementById("cont").style.display = "none";
+        return show2 = 0;
+    }
+    else
+        document.getElementById("cont").style.display = "block";
+        return show2 = 1;
+
+}
+var show3;
+function show_hide4() {
+    if (show3 == 1) {
+        document.getElementById("sat").style.display = "none";
+        return show3 = 0;
+    }
+    else
+        document.getElementById("sat").style.display = "block";
+        return show3 = 1;
+
+}
+var show4;
+function show_hide5() {
+    if (show4 == 1) {
+        document.getElementById("inv").style.display = "none";
+        return show4 = 0;
+    }
+    else
+        document.getElementById("inv").style.display = "block";
+        return show4 = 1;
+
+}
+var show5;
+function show_hide6() {
+    if (show5 == 1) {
+        document.getElementById("fade").style.display = "none";
+        return show5 = 0;
+    }
+    else
+        document.getElementById("fade").style.display = "block";
+        return show5 = 1;
+
+}
+var show6;
+function show_hide7() {
+    if (show6 == 1) {
+        document.getElementById("zoom").style.display = "none";
+        return show6 = 0;
+    }
+    else
+        document.getElementById("zoom").style.display = "block";
+        return show6 = 1;
+
+}
+var show7;
+function show_hide8() {
+    if (show7 == 1) {
+        document.getElementById("flip").style.display = "none";
+        return show7 = 0;
+    }
+    else
+        document.getElementById("flip").style.display = "block";
+        return show7 = 1;
+
+}
+var show8;
+function show_hide9() {
+    if (show8 == 1) {
+        document.getElementById("3d").style.display = "none";
+        return show8 = 0;
+    }
+    else
+        document.getElementById("3d").style.display = "block";
+        return show8 = 1;
+
+}
