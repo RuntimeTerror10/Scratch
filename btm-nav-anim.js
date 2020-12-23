7b5500ae72066be66bba2781deb64a1773ff5fc0
Parth Bhardwaj <53986600+RuntimeTerror10@users.noreply.github.com>
2020-12-23 14:41:02 +0530

    navbar padding and btm-nav  fix

--- btm-nav-anim.js
+++ btm-nav-anim.js
@@ -2,19 +2,20 @@ var arrow = document.getElementById('ar');
 var links = document.getElementById('nv');
 var arr =document.getElementById('a');
 
-var show;
+var show = false;
 
 function pop(){
-    if(show == 1){
+    console.log(999)
+    if(!show){
         arrow.classList.add('btm-arrow-active');
         links.classList.add('btm-nav-active');
         arr.classList.add('arrow-active');
-        return show = 0; 
+        return show = true; 
     }
     else{
         arrow.classList.remove('btm-arrow-active');
         links.classList.remove('btm-nav-active');
         arr.classList.remove('arrow-active');
-        return show = 1; 
+        return show = false; 
     }
 }
