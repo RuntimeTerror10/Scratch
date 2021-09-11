3f77498f3af54aed2759d492a5383768d2969e17
RuntimeTerror10 <parthb.git@gmail.com>
2021-09-11 17:35:39 +0530

    reset 11ty setup

--- .eleventy.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("scripts");
-};
--- _data/nav.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-  { label: "Home", url: "/" },
-  { label: "Docs", url: "/docs" },
-  { label: "About", url: "/about" },
-];
