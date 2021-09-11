7c58ca7b27dcf3fe98748e8d090e1843f5d378c3
RuntimeTerror10 <parthb.git@gmail.com>
2021-09-11 16:26:54 +0530

    initial commit

--- index.js
+++
@@ -1,4 +0,0 @@
-const navbar = document.querySelector('nav');
-function change(){
-    navbar.style.left='0';
-}
--- preloader.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-    const preloader = document.querySelector('.preloader');
-    preloader.classList.add('preloader-finish');
-})
-
-window.addEventListener('load', () => {
-    const an = document.querySelector('.no_ani');
-    an.classList.add('circles');
-})
